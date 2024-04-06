1f29c972ab11cbd83b9bd164b9f5608758e3bce7
DESKTOP-TUDPHM1\Tonmoy <tonmoy.debnath@northsouth.edu>
2024-04-06 07:38:44 +0600

    reservation

--- index.js
+++ index.js
@@ -248,7 +248,7 @@ async function run(){
             const mailOption={
               from: process.env.EMAIL,
               to: 'tonmoydebnath192@gmail.com',
-              subject: `${message.subject}`,
+              subject: `For Reservation`,
               html:`
               <!DOCTYPE HTML PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
                 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml" xmlns:o="urn:schemas-microsoft-com:office:office">
